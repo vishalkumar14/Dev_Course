c87062d336b3274ac0f66182e07bff954e1dbf26
Vishal Kumar <vishalipu14@gmail.com>
2019-10-31 16:34:17 +0530

    New Code Added

--- Dev_Course_Fast/Nodejs/api.js
+++ Dev_Course_Fast/Nodejs/api.js
@@ -6,6 +6,8 @@ const planRouter = require("./router/planRouter");
 
 app.use(express.json());
 app.use(express.static("public"));
+app.set("view engine", "pug");
+app.set("views", "views");
 
 app.use("/api/user", userRouter);
 app.use("/api/plan", planRouter);
