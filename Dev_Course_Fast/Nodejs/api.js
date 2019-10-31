34435dbc342d71d5c9845a74e9aae0de44869d10
Vishal Kumar <vishalipu14@gmail.com>
2019-10-31 22:33:42 +0530

    nn

--- Dev_Course_Fast/Nodejs/api.js
+++ Dev_Course_Fast/Nodejs/api.js
@@ -9,7 +9,23 @@ app.use(express.static("public"));
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.get("/", function(req, res){
+    res.status(200).render("base.pug");
+})
+
 app.use("/api/user", userRouter);
 app.use("/api/plan", planRouter);
 
 module.exports = app;
+
+
+// Router
+// \
+// \login
+// \signup
+// \home
+// \plan
+// \planpage
+// \reset
+// \forgetpassword
+// \userpage
